4eba575ddffab22294251123ebda5adecc6a52da
ErikMachuca999 <mac19024@byui.edu>
2021-06-12 12:31:00 -0500

    made ceiling collision

--- batter_template/batter/game/handle_collisions_action.py
+++ batter_template/batter/game/handle_collisions_action.py
@@ -1,6 +1,8 @@
 import random
 from game import constants
 from game.action import Action
+from game.point import Point
+
 
 class HandleCollisionsAction(Action):
     """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
@@ -15,11 +17,29 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
-        # marquee = cast["marquee"][0] # there's only one
+        
         paddle = cast["paddle"][0] # there's only one
         bricks = cast["brick"]
-        # marquee.set_text("")
-        for brick in bricks:
-            if paddle.get_position().equals(brick.get_position()):
-                description = brick.get_description()
-                # marquee.set_text(description) 
+        ball = cast["ball"][0]
+
+        # for brick in bricks:
+        #     if ball.get_position().equals(brick.get_position()):
+        #         description = brick.get_description()
+        
+        # ceiling collision
+        for x in range(0, constants.MAX_X):
+            if ball.get_position().equals(Point(x, 1)):
+                velocity = ball.get_velocity()
+                ball.set_velocity(Point(velocity.get_x(), velocity.get_y()*-1))
+        # left wall
+        # for x in range(0, constants.MAX_X):
+        #     if ball.get_position().equals(Point(x, 1)):
+        #         velocity = ball.get_velocity()
+        #         ball.set_velocity(Point(velocity.get_x(), velocity.get_y()*-1))
+
+
+
+
+        for x in range(0, constants.MAX_Y):
+            if ball.get_position().equals(Point(20, x)):
+                quit()
